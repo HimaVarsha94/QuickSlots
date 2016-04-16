2ae307d81bfe3fdee8e1b4557289bba2a7cc7b25
Sanyam Kapoor <1sanyamkapoor@gmail.com>
2016-04-16 19:14:23 +0530

    added sample config

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+  $config = array (
+  'db_host' => '127.0.0.1',
+  'db_name' => 'quickslots',
+  'db_user' => 'root',
+  'db_pswd' => '123',
+  'ldap_host' => '',
+  'ldap_dn' => '',
+);
+?>
