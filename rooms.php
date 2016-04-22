f21c86495002f12af72a6d4c97d45b0e1e9367da
gupta-abhay <abhay94gupta@gmail.com>
2016-04-22 18:33:53 +0530

    Updated room features

--- rooms.php
+++ rooms.php
@@ -13,10 +13,13 @@ rangeCheck('room_name',2,25);
 if(valueCheck('action','add'))
 {
   rangeCheck('capacity',1,3);
-  try{
-    $query = $db->prepare('INSERT INTO rooms(room_name,capacity) values (?,?)');
-    $query->execute([$_POST['room_name'],$_POST['capacity']]);
-    postResponse("addOpt","Room Added",[$_POST['room_name'],$_POST['capacity']]);    
+  if(empty($_POST['isLab']))
+    $_POST['isLab'] = 0;
+  try
+  {
+    $query = $db->prepare('INSERT INTO rooms(room_name,capacity,lab) values (?,?,?)');
+    $query->execute([$_POST['room_name'],$_POST['capacity'],$_POST['isLab']]);
+    postResponse("addOpt","Room Added",[$_POST['room_name'],$_POST['capacity'],$_POST['isLab']]);
   }
   catch(PDOException $e)
   {
@@ -25,8 +28,17 @@ if(valueCheck('action','add'))
     else
       postResponse("error",$e->errorInfo[2]);
   }
-  
+
 }
+elseif(valueCheck('action', 'update'))
+{
+  if(empty($_POST['lab']))
+    $_POST['lab'] = 0;
+  $query = $db->prepare('UPDATE rooms SET capacity=:capacity, lab=:lab where room_name=:room_name');
+  $query->execute(array(':capacity' => $_POST['capacity'], ':room_name' => $_POST['room_name'], ':lab' => $_POST['updateLab']));
+  // postResponse("updateOpt", "Room Updated", [$_POST['room_name']])
+}
+
 elseif(valueCheck('action','delete'))
 {
   $query = $db->prepare('DELETE FROM rooms where room_name = ?');
