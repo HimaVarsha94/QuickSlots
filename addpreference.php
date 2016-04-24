41a36bf50cba279a0ef57592a3d901cde11b0630
Adarsh Pugalia <adarsh.pugaliaa@gmail.com>
2016-04-24 16:05:31 +0530

    Added backend for delete preferences. Reload added to delete.

--- addpreference.php
+++ addpreference.php
@@ -558,7 +558,7 @@ HTML;
     <div class="avatar"><div class="icon remove"></div></div>
     <div class="title">Delete Preference</div>
     <div class="elements">
-      <form method="post" action="preferences.php?action=del" class="confirm">
+      <form method="post" action="preferences.php?action=del" class="confirm" onsubmit="setTimeout(function () { window.location.reload(); }, 10)">
         <select name="preference_id" class="updateSelect stretch" data-placeholder="Choose Preference..." required>
           <option label="Choose Preference..."></option>
           <?php
--- preferences.php
+++ preferences.php
@@ -7,12 +7,20 @@ require_once('connect_db.php');
 
 // echo '{"course_id": "'.$_POST["course_id"].'", "Slot1" : "'.$_POST["course_slot1"].'",  "Slot2" : "'.$_POST["course_slot2"].'", "Slot3" : "'.$_POST["course_slot3"].'"}';
 
-$query = $db -> prepare('INSERT INTO preferences(course_id, slot_group) VALUES (?,?) ');
-if(isset($_POST["course_slot1"]) && $_POST["course_slot1"] != "select_slot")
-    $query -> execute([$_POST["course_id"], $_POST["course_slot1"]]);
-if(isset($_POST["course_slot2"]) && $_POST["course_slot2"] != "select_slot")
-    $query -> execute([$_POST["course_id"], $_POST["course_slot2"]]);
-if(isset($_POST["course_slot3"]) && $_POST["course_slot3"] != "select_slot")
-    $query -> execute([$_POST["course_id"], $_POST["course_slot3"]]);
-echo '{"Success": "True"}';
+if(valueCheck('action','del'))
+{
+    $query = $db->prepare('DELETE FROM preferences WHERE id = ?');
+    $query->execute([$_POST['preference_id']]);
+    postResponse("addOpt","Slot Group Deleted.",[$_POST['preference_id']]);    
+}
+else {
+	$query = $db -> prepare('INSERT INTO preferences(course_id, slot_group) VALUES (?,?) ');
+	if(isset($_POST["course_slot1"]) && $_POST["course_slot1"] != "select_slot")
+	    $query -> execute([$_POST["course_id"], $_POST["course_slot1"]]);
+	if(isset($_POST["course_slot2"]) && $_POST["course_slot2"] != "select_slot")
+	    $query -> execute([$_POST["course_id"], $_POST["course_slot2"]]);
+	if(isset($_POST["course_slot3"]) && $_POST["course_slot3"] != "select_slot")
+	    $query -> execute([$_POST["course_id"], $_POST["course_slot3"]]);
+	echo '{"Success": "True"}';
+}
  ?>
